8e71eb08f3a54b9840d0b6e507d95c4688292eb3
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-09-13 10:58:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+
+  ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Pikachu',
+        description: 'Petite créature jaune très mignonne mais électrique !',
+        createdDate: new Date(),
+        snaps: 10,
+        imageUrl:
+          'https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png',
+        location: 'Bourg-Palette',
+      },
+      {
+        title: 'Carapuce',
+        description:
+          'Pokemon de type eau à choisir au début du jeu... I love Tortank !',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl:
+          'https://www.pokepedia.fr/images/thumb/c/cc/Carapuce-RFVF.png/250px-Carapuce-RFVF.png',
+        location: 'Azur',
+      },
+      {
+        title: 'Salameche',
+        description:
+          "Première évolution pokemon avant d'avoir reptincelle et DRACEUFEUUUUU !",
+        createdDate: new Date(),
+        snaps: 350,
+
+        imageUrl:
+          'https://icon-library.com/images/charmander-icon/charmander-icon-4.jpg',
+      },
+    ];
+  }
 }
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+
+  buttonText!: string;
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snap !';
+  }
+
+  onSnap() {
+    return this.buttonText === 'Oh Snap !'
+      ? this.faceSnap.snaps++ && (this.buttonText = 'Oops, unSnap !')
+      : this.faceSnap.snaps-- && (this.buttonText = 'Oh Snap !');
+  }
+}
